6e9ad277a12601e5f0d8f72644df74aa01df71db
Alexander Chabowski <alexader.chabowski@mbq-gmbh.de>
2024-11-28 15:36:14 +0100

    GameTime

--- src/game.c
+++ src/game.c
@@ -13,16 +13,18 @@ const Color COLOR_DIE_NEXT = {170, 170, 170, 255};
 #define GRID_HEIGHT 90
 #define GRID_WIDTH 160
 
-// Declare static variables for the game state
-static int **cells;
-static int **updated_cells;
+static int** cells;
+static int** updated_cells;
 static RenderTexture2D screen;
 static bool running = false;
+static float updateTime = 0.0f;
+static constexpr float updateInterval = 0.05f;
 
 // Initialize the game
-void InitGame(const int screenWidth, const int screenHeight) {
+void InitGame(const int screenWidth, const int screenHeight)
+{
     InitWindow(screenWidth, screenHeight, "Raylib - Conways Game of Life");
-    SetTargetFPS(25);
+    SetTargetFPS(120);
 
     // Create arrays to store the current and next generation of cells
     cells = createArray(GRID_HEIGHT, GRID_WIDTH);
@@ -34,22 +36,27 @@ void InitGame(const int screenWidth, const int screenHeight) {
 }
 
 // Update the game state
-void UpdateGame(void) {
+void UpdateGame(void)
+{
     const int screenWidth = GetScreenWidth();
     const int screenHeight = GetScreenHeight();
+    float deltaTime = GetFrameTime();
 
     // Toggle running state with space key
-    if (IsKeyReleased(KEY_SPACE)) {
+    if (IsKeyReleased(KEY_SPACE))
+    {
         running = !running;
         update(screen, cells, updated_cells, 10, false);
     }
 
     // Draw alive cells with left mouse button
-    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON)) {
+    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+    {
         const Vector2 pos = GetMousePosition();
         const int x = (int)pos.x;
         const int y = (int)pos.y;
-        if ((x >= 0 && x < screenWidth) && (y >= 0 && y < screenHeight)) {
+        if ((x >= 0 && x < screenWidth) && (y >= 0 && y < screenHeight))
+        {
             cells[y / 10][x / 10] = 1;
             update(screen, cells, updated_cells, 10, false);
         }
@@ -58,10 +65,12 @@ void UpdateGame(void) {
     // Generate random pattern with right mouse button
     if (IsMouseButtonDown(MOUSE_RIGHT_BUTTON))
     {
-        srand((unsigned int)time(nullptr)); // Seed the random number generator
+        srand((unsigned int)time(NULL)); // Seed the random number generator
 
-        for (int i = 0; i < GRID_HEIGHT; i++) {
-            for (int j = 0; j < GRID_WIDTH; j++) {
+        for (int i = 0; i < GRID_HEIGHT; i++)
+        {
+            for (int j = 0; j < GRID_WIDTH; j++)
+            {
                 cells[i][j] = rand() % 2; // Generate random number (0 or 1)
             }
         }
@@ -69,19 +78,26 @@ void UpdateGame(void) {
     }
 
     // Update cells if the game is running
-    if (running) {
-        copyArray(cells, update(screen, cells, updated_cells, 10, true), GRID_HEIGHT, GRID_WIDTH);
+    if (running)
+    {
+        updateTime += deltaTime;
+        if (updateTime >= updateInterval)
+        {
+            copyArray(cells, update(screen, cells, updated_cells, 10, true), GRID_HEIGHT, GRID_WIDTH);
+            updateTime = 0.0f;
+        }
     }
 }
 
 // Draw the game state
-void DrawGame(void) {
+void DrawGame(void)
+{
     BeginDrawing();
-    ClearBackground(BLACK);
     DrawTextureRec(screen.texture, (Rectangle){0, 0, GetScreenWidth(), -GetScreenHeight()}, (Vector2){0, 0}, WHITE);
 
     // Display instructions when the game is not running
-    if (!running) {
+    if (!running)
+    {
         const Color customColor = ColorFromHex("25D36F");
         DrawText("Press MOUSE LEFT  to draw alive cells", 10, 10, 20, customColor);
         DrawText("Press MOUSE RIGHT to generate random pattern", 10, 30, 20, customColor);
@@ -92,7 +108,8 @@ void DrawGame(void) {
 }
 
 // Unload the game resources
-void UnloadGame(void) {
+void UnloadGame(void)
+{
     destroyArray(cells);
     destroyArray(updated_cells);
     UnloadRenderTexture(screen);
@@ -100,31 +117,44 @@ void UnloadGame(void) {
 }
 
 // Update the cells based on Conway's Game of Life rules
-int **update(const RenderTexture2D screen, int **cells, int **updated_cells, const int size, const bool with_progress) {
+int** update(const RenderTexture2D screen, int** cells, int** updated_cells, const int size, const bool with_progress)
+{
     fillArray(updated_cells, GRID_HEIGHT, GRID_WIDTH, 0);
 
     BeginTextureMode(screen);
     ClearBackground(COLOR_GRID);
-    for (int row = 0; row < GRID_HEIGHT; row++) {
-        for (int col = 0; col < GRID_WIDTH; col++) {
+    for (int row = 0; row < GRID_HEIGHT; row++)
+    {
+        for (int col = 0; col < GRID_WIDTH; col++)
+        {
             const int alive = sumArray(cells, GRID_HEIGHT, GRID_WIDTH, (Range){row - 1, col - 1, 3, 3}) - cells[row][col];
             Color color = cells[row][col] == 0 ? COLOR_BG : BLUE;
 
-            if (cells[row][col] == 1) {
-                if (alive < 2 || alive > 3) {
-                    if (with_progress) {
+            if (cells[row][col] == 1)
+            {
+                if (alive < 2 || alive > 3)
+                {
+                    if (with_progress)
+                    {
                         color = COLOR_DIE_NEXT;
                     }
-                } else {
+                }
+                else
+                {
                     updated_cells[row][col] = 1;
-                    if (with_progress) {
+                    if (with_progress)
+                    {
                         color = BLUE;
                     }
                 }
-            } else {
-                if (alive == 3) {
+            }
+            else
+            {
+                if (alive == 3)
+                {
                     updated_cells[row][col] = 1;
-                    if (with_progress) {
+                    if (with_progress)
+                    {
                         color = BLUE;
                     }
                 }
@@ -139,7 +169,8 @@ int **update(const RenderTexture2D screen, int **cells, int **updated_cells, con
 }
 
 // Convert a hex color code to a Color object
-Color ColorFromHex(const char *hex) {
+Color ColorFromHex(const char* hex)
+{
     int r, g, b;
     sscanf(hex, "%02x%02x%02x", &r, &g, &b);
     return (Color){r, g, b, 255};
